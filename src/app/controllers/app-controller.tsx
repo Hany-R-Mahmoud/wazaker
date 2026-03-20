f701dee8157d41da2dd8f85f8ff34e4372f95df6
Hany Mahmoud <hany.r.mahmoud@gmail.com>
2026-03-20 21:17:27 +0200

    chore: update app controller

--- src/app/controllers/app-controller.tsx
+++ src/app/controllers/app-controller.tsx
@@ -1,5 +1,5 @@
-import { RecitationHomeView } from '../../features/recitation/views/recitation-home-view';
+import { RecordingScreen } from '../../features/recitation/screens/recording-screen';
 
 export function AppController() {
-  return <RecitationHomeView />;
+  return <RecordingScreen />;
 }
